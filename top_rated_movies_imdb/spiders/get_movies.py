680b21d776c226a724cf7129881049b88f78799d
david1707 <DavidAlacant@gmail.com>
2018-09-23 19:20:54 +0200

    Absolute page url added

--- top_rated_movies_imdb/spiders/get_movies.py
+++ top_rated_movies_imdb/spiders/get_movies.py
@@ -7,21 +7,24 @@ class GetMoviesSpider(scrapy.Spider):
     allowed_domains = ['www.imdb.com/chart/top?ref_=nv_mv_250']
     start_urls = ['https://www.imdb.com/chart/top?ref_=nv_mv_250/']
 
+
     def parse(self, response):
         movies = response.xpath('//tbody/tr')
-
 
         for movie in movies:
             order = movie.xpath('.//@data-value').extract_first()
             relative_page_url = movie.xpath('.//a/@href').extract_first()
+            absolute_page_url = 'www.imdb.com' + relative_page_url
             title = movie.xpath('.//*[@class="titleColumn"]/a/text()').extract_first()
             year = movie.xpath('.//*[@class="titleColumn"]/span/text()').extract_first()
             rating = movie.xpath('.//*[@class="ratingColumn imdbRating"]/strong/text()').extract_first()
 
-            print('*'* 50)
+            print(absolute_page_url)
+
             yield{
-                'order': order,
-                'title': title,
-                'year': year,
-                'rating': rating
+                'Order': order,
+                'Title': title,
+                'Year': year,
+                'Rating': rating,
+                'Movie_url': absolute_page_url
             }
